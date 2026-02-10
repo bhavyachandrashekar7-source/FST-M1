1166f3231a14b23bb73d832c97f6fba896e30844
bhavyachandrashekar7-source <bhavyachandrashekar7@gmail.com>
2026-02-10 11:32:13 +0530

    Add files via upload

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,16 @@
+package Activities;
+
+public class activity1 {
+	public static void main(String[] args) {
+        Car Scorpio = new Car();
+        Scorpio.make = 2014;
+        Scorpio.color = "Black";
+        Scorpio.transmission = "Manual";
+    
+     
+        Scorpio.displayCharacterstics();
+        Scorpio.accelerate();
+        Scorpio.brake();
+    }
+
+}
